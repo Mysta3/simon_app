d1da595c0242c948d697552f6f83cd37af297f30
Mysta3 <myshawnestallings@gmail.com>
2020-01-06 09:42:19 -0800

    added start game functionality & glow functionality.

--- simon_app/script.js
+++ simon_app/script.js
@@ -1,7 +1,37 @@
+console.log('server started');
+
 //Start Display
 
-/*
-1. select button.
-2. when user clicks change start-panel to display: none
-3. change game-panel to display: inline-block or opacity 1.
-*/
+//select button.
+const startButton = document.querySelector('.start-panel');
+//select game-panel
+const gamePanel = document.querySelector('.game-panel');
+//when user clicks change start-panel to display: none
+// startButton.addEventListener('click', startGame);
+//3. change game-panel to display: inline-block or opacity 1.
+
+//START GAME FUNCTION
+//gamePanel.style.opacity = 0; //set opacity of game panel to 0.
+function startGame(evt) {
+  console.log(evt.target);
+
+  if ((startButton.style.opacity = 1)) {
+    startButton.style.opacity = 0;
+    gamePanel.style.opacity = 1;
+  }
+}
+/****************************************/
+
+//Colors Light Up
+//add click event to to game-panel
+gamePanel.addEventListener('click', colorLightUp);
+
+//box-shadow: h-offset, v-offset, blur, spread, color
+function colorLightUp(evt) {
+  let colorDiv = evt.target;
+  let hasColor = colorDiv.dataset.color;
+  if (hasColor) {
+    colorDiv.style.boxShadow = ` 0px 0px 30px 10px ${hasColor}`;
+    console.log(hasColor);
+  }
+}
