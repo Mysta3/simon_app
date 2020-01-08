0ccb83a834c554b225d2386de9f9e995f8d2a5ec
Mysta3 <myshawnestallings@gmail.com>
2020-01-08 10:49:29 -0800

    v1 Logic finished.

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ const gameOverPanel = document.querySelector('.gameOver');
 const colorsArray = document.querySelectorAll('.color');
 const instructions = document.querySelector('.instructions');
 const hiddenInstructions = document.querySelector('.lists');
+const tester = document.querySelector('.tester');
 let computerArr = []; //computer's Array
 let userColorPicks = []; //user's array
 
-gamePanel.style.opacity = 1;
-gameOverPanel.style.opacity = 1;
+gamePanel.style.opacity = 0;
+gameOverPanel.style.opacity = 0;
 
 instructions.addEventListener('click', showInstructions);
 startButton.addEventListener('click', startGame);
@@ -24,31 +25,34 @@ function startGame() {
   startButton.style.opacity = 0;
   gamePanel.style.opacity = 1; //change game-panel to opacity 1
   gameOverPanel.style.opacity = 0;
+  instructions.style.opacity = 1;
+  emptyArrays();
   randomize();
 }
 
 //randomize color & push to computerArray
 function randomize() {
-  // console.log('randomize func');
   // reference: https://medium.com/@fyoiza/how-to-randomize-an-array-in-javascript-8505942e452
   let ranNum = Math.floor(Math.random() * colorsArray.length); //produce random index
   let colors = colorsArray[ranNum];
   let colorName = colors.innerText;
   computerArr.push(colorName);
+  console.log(colors);
   //reference end//
-  console.log(computerArr);
+
   //initiates glow
   computerArr.forEach(function(colorIndex, time) {
-    console.log(time);
-    let color = colorIndex; //target innerText
+    // console.log(colorIndex);
+
+    // let color = colorIndex; //target innerText
     setTimeout(() => {
       setTimeout(() => {
-        colors.classList.toggle(`glowEffect${color}`);
+        tester.classList.add(`glowEffect${colorIndex}`);
         setTimeout(() => {
           //if glow is on turn off glow
-          colors.classList.toggle(`glowEffect${color}`);
+          tester.classList.remove(`glowEffect${colorIndex}`);
         }, 500);
-      }, 1000);
+      }, 300);
     }, time * 1000);
   });
 }
@@ -59,81 +63,77 @@ function userInput(evt) {
   let hasColor = colorDiv.dataset.color;
 
   if (hasColor) {
-    console.log('user input func');
     if (hasColor === 'black') {
       userColorPicks.push(hasColor);
-      colorDiv.classList.toggle(`glowEffectBlack`);
-      checkFunc();
+      colorDiv.classList.add(`glowEffectBlack`);
+      // checkFunc();
       setTimeout(() => {
-        if (colorDiv.classList.toggle('glowEffectBlack')) {
-          colorDiv.classList.toggle('glowOff');
-        }
+        colorDiv.classList.remove('glowEffectBlack');
       }, 500);
     } else if (hasColor === 'green') {
       userColorPicks.push(hasColor);
-      colorDiv.classList.toggle(`glowEffectGreen`);
-      checkFunc();
+      colorDiv.classList.add(`glowEffectGreen`);
+      // checkFunc();
       setTimeout(() => {
-        if (colorDiv.classList.toggle('glowEffectGreen')) {
-          colorDiv.classList.toggle('glowOff');
-        }
+        colorDiv.classList.remove(`glowEffectGreen`);
       }, 500);
     } else if (hasColor === 'orange') {
       userColorPicks.push(hasColor);
-      colorDiv.classList.toggle(`glowEffectOrange`);
-      checkFunc();
+      colorDiv.classList.add(`glowEffectOrange`);
+      // checkFunc();
       setTimeout(() => {
-        if (colorDiv.classList.toggle('glowEffectOrange')) {
-          colorDiv.classList.toggle('glowOff');
-        }
+        colorDiv.classList.remove(`glowEffectOrange`);
       }, 500);
     } else if (hasColor === 'purple') {
       userColorPicks.push(hasColor);
-      colorDiv.classList.toggle(`glowEffectPurple`);
-      checkFunc();
+      colorDiv.classList.add(`glowEffectPurple`);
+      // checkFunc();
       setTimeout(() => {
-        if (colorDiv.classList.toggle('glowEffectPurple')) {
-          colorDiv.classList.toggle('glowOff');
-        }
+        colorDiv.classList.remove(`glowEffectPurple`);
       }, 500);
     }
-    //place push array outside of inside if
-    //if length equals each other
-    //then run checkfunc
   }
-  console.log(userColorPicks);
+  // for (let i = 0; i < userColorPicks.length; i++) {
+    if (computerArr.length === userColorPicks.length) {
+    console.log(computerArr);
+    console.log(userColorPicks);
+    checkFunc();
+    // }
+  }
+
+  // randomize();
 } //end of userInput Function
 
 function checkFunc() {
-  // console.log(userColorPicks);
-  // console.log(computerArr);
-  // console.log('checkFunc');
-  if (userColorPicks.length === computerArr.length) {
-    for (let i = 0; i < computerArr.length; i++) {
-      for (let x = 0; x < userColorPicks.length; x++) {
-        if (computerArr[i].toLowerCase() === userColorPicks[x].toLowerCase()) {
-          // console.log(computerArr[i].toLowerCase());
-          // console.log(userColorPicks[x].toLowerCase());
-        } else {
-          gameOver();
-        }
-      }
+  let isGameOver = false;
+  //reference https://www.geeksforgeeks.org/how-to-compare-two-arrays-in-javascript/
+  for (let i = 0; i < computerArr.length; i++) {
+    console.log(computerArr[i], userColorPicks[i]);
+    if (computerArr[i].toLowerCase() !== userColorPicks[i].toLowerCase()) {
+      console.log('Not equal!');
+      isGameOver = true;
     }
-    // randomize();
   }
+  if (isGameOver) {
+    gameOver();
+    return;
+  }
+  userColorPicks = [];
+  randomize();
+  // }
 }
 
 //GAME OVER FUNCTION
 function gameOver() {
-  console.log('game over!');
+  emptyArrays();
+  // console.log('game over!');
   startButton.style.opacity = 1;
   gamePanel.style.opacity = 0; //change game-panel to opacity 1
   gameOverPanel.style.opacity = 1;
   instructions.style.opacity = 0;
-  if (computerArr.length > 0) {
-    computerArr = []; //empty current Array
-  }
-  if (userColorPicks.length > 0) {
-    userColorPicks = []; //empty current Array
-  }
+}
+
+function emptyArrays() {
+  computerArr = []; //empty current Array
+  userColorPicks = []; //empty current Array
 }
